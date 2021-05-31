bbfe24a22b41326d48ca7e50c268799860657519
mcgear <michael.gearhardt@gmail.com>
2021-05-31 11:28:06 -0600

    Adjusted home page

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,38 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Fun',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    description: <>If it isn't fun, it isn't worth doing. We want everyone to have fun the whole time they're here.</>,
+  },
+  {
+    title: 'Active',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Kids have fun when they are being active.  Practices and games will keep the kids active physically and mentally.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
+    title: 'Engaged',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
+      <>Fun + Active = Engaged.  In order to learn, you have to be engaged and that's the goal for our season.</>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
