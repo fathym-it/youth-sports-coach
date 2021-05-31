409e4be1b2caf08382b914a16127c2f9744ec081
mcgear <michael.gearhardt@gmail.com>
2021-05-31 09:13:10 -0600

    Checkpoint

--- docs/intro.md
+++ docs/intro.md
@@ -2,9 +2,9 @@
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# Season Introduction
 
-Let's discover **Docusaurus in less than 5 minutes**.
+Welcome to the 2021 Erie Rec Baseball season (5U Tee Ball )  
 
 ## Getting Started
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '5U Tee Ball & 7U Coach Pitch',
+  tagline: 'Coach Gearhardt',
+  url: 'https://coach.gearhardt.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -11,21 +11,21 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Teams should be Fun!',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Teams should be Fun!',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/c/c0/Baseball_%28crop%29_transparent.png',
       },
       items: [
         {
           type: 'doc',
           docId: 'intro',
           position: 'left',
-          label: 'Tutorial',
+          label: 'The Season',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/fafathym-it/youth-sports-coach',
           label: 'GitHub',
           position: 'right',
         },
@@ -38,25 +38,8 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'The Season',
               to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
             },
           ],
         },
@@ -69,12 +52,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/fathym-it/youth-sports-coach',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Youth Sports Coach. Built with Docusaurus and Fathym.`,
     },
   },
   presets: [
@@ -85,13 +68,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/fathym-it/youth-sports-coach/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/fathym-it/youth-sports-coach/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
